aa8a4315e57747537f5039377570028f5e49247d
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2024-01-03 13:38:25 +0000

    databases creation

---
+++ infraestructure/modules/databases/main.tf
@@ -0,0 +1,31 @@
+resource "aws_dynamodb_table" "datamart" {
+  name           = var.datamart_table_name
+  billing_mode   = "PAY_PER_REQUEST" #Cambiar según necesidades del cliente
+  hash_key       = "token_id"
+
+  attribute {
+    name = "token_id"
+    type = "S"
+  }
+  tags = {
+    Name = var.datamart_table_name
+  }
+
+}
+
+resource "aws_dynamodb_table" "clients" {
+  name           = var.clients_table_name
+  billing_mode   = "PAY_PER_REQUEST" #Cambiar según necesidades del cliente
+  hash_key       = "client_id"
+  
+  attribute {
+    name = "client_id"
+    type = "S"
+  }
+
+  tags = {
+    Name = var.clients_table_name
+  }
+}
+
+#Posible modificaciones
--- infraestructure/outputs.tf
+++ infraestructure/outputs.tf
@@ -1,3 +1,11 @@
 output "buckets_names" {
   value = module.buckets.bucket_names
 }
+
+output "datamart_table_name" {
+  value = module.databases.datamart_table_name
+}
+
+output "clients_table_name" {
+  value = module.databases.clients_table_name
+}
