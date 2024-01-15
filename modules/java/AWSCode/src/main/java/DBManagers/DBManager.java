506b60ced0e6450f8f7795adeee5b976ed26a9e5
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2024-01-15 18:20:24 +0000

    DynamoDBManager creation

--- modules/java/AWSCode/src/main/java/DBManagers/DBManager.java
+++ modules/java/AWSCode/src/main/java/DBManagers/DBManager.java
@@ -1,10 +1,15 @@
 package DBManagers;
 
+import java.util.HashMap;
+import java.util.List;
+
 public interface DBManager {
-    void initializeManager(String DB, String table) throws Exception;
-    void add(String data) throws Exception;
+    void initializeManager(List<String> dbDetails) throws Exception;
+    void closeManager();
+    void add(HashMap data) throws Exception;
     void delete(String id) throws Exception;
-    void update(String id, String newData) throws Exception;
-    String query(String id) throws Exception;
+    void update(String id, HashMap newData) throws Exception;
+    List query(String id) throws Exception;
+
 
 }
