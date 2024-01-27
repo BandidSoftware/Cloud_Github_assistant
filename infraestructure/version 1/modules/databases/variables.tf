8edf3b858545297b5e067e319e0213f78dc213ca
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2024-01-27 17:11:16 +0000

    old infraestructure deletion

--- infraestructure/version 1/modules/databases/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "datamart_table_name" {
-  description = "Name of the DynamoDB table for DataMart"
-  type        = string
-}
-
-variable "clients_table_name" {
-  description = "Name of the DynamoDB table for Clients"
-  type        = string
-}
